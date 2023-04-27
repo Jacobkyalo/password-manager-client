f7b82d85644356e54e3ddeee96552629c158e337
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-04-27 21:58:31 +0300

    reduced text size

--- src/pages/home/components/footer/index.jsx
+++ src/pages/home/components/footer/index.jsx
@@ -7,26 +7,26 @@ const index = () => {
       <hr className="mx-auto mb-10 block border-2" />
       <div className="flex flex-col items-start justify-center gap-y-10">
         <div className="flex flex-col gap-y-6">
-          <img src={logo} alt={logo} className="block w-full max-w-[300px] text-start" />
+          <img src={logo} alt={logo} className="block w-full max-w-[200px] text-start" />
           <div className="ps-2">
-            <h4 className="mb-6 text-[30px] font-medium">Reach Creator</h4>
+            <h4 className="mb-6 text-[20px] font-medium">Reach Creator</h4>
             <div className="flex gap-x-6">
-              <BsTwitter size={30} />
-              <BsLinkedin size={30} />
-              <BsGithub size={30} />
+              <BsTwitter size={20} />
+              <BsLinkedin size={20} />
+              <BsGithub size={20} />
             </div>
           </div>
         </div>
         <div className="ps-2">
-          <h4 className="mb-6 text-[30px] font-medium">Contribute</h4>
-          <p className="text-[20px] opacity-80">Raise an issue</p>
-          <p className="text-[20px] opacity-80">Open a pull request</p>
-          <p className="text-[20px] opacity-80">
+          <h4 className="mb-6 text-[20px] font-medium">Contribute</h4>
+          <p className="text-[17px] opacity-80">Raise an issue</p>
+          <p className="text-[17px] opacity-80">Open a pull request</p>
+          <p className="text-[17px] opacity-80">
             Check code on <span className="text-violet-600">GitHub</span>
           </p>
         </div>
       </div>
-      <p className="mt-6 ps-2 text-start text-[18px] opacity-80">&copy; Password Manager. All rights reserved</p>
+      <p className="mt-6 ps-2 text-start text-[17px] opacity-80">&copy; Password Manager. All rights reserved</p>
     </footer>
   );
 };
