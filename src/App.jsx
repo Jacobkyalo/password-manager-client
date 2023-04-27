88b9433fbbbc2b3defa047a525ec3a2d69017a34
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-04-27 21:59:22 +0300

    rendered pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Home from './pages/home';
+import Dashboard from './pages/dashboard';
+import SignUp from './pages/signup';
+import Login from './pages/login';
+import ForgotPassword from './pages/forgot-password';
+import NewPassword from './pages/add-password';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-center text-3xl text-blue-600">Password Manager</h1>
+    <div className="px-[10px]">
+      {/* <Home /> */}
+      {/* <Dashboard /> */}
+      <SignUp />
+      {/* <Login /> */}
+      {/* <ForgotPassword /> */}
+      {/* <NewPassword /> */}
     </div>
   );
 }
