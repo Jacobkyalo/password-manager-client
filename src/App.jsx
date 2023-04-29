c1f27477894cd6e6345aba6d344ed047e719089d
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-04-29 10:41:28 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import Dashboard from './pages/dashboard';
 import SignUp from './pages/signup';
@@ -8,12 +9,16 @@ import NewPassword from './pages/add-password';
 function App() {
   return (
     <div className="px-[10px]">
-      {/* <Home /> */}
-      {/* <Dashboard /> */}
-      <SignUp />
-      {/* <Login /> */}
-      {/* <ForgotPassword /> */}
-      {/* <NewPassword /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/add-password" element={<NewPassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
