da83506fc04e50045b8047b6af374aeb5ce916f1
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-05-23 17:49:59 +0300

    configured redux store

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../slices/userSlice';
+import { apiSlice } from '../slices/apiSlice';
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true
+});
+
+export default store;
