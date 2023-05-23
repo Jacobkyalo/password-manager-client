23e606ef1fdaac1b4ebfafef6afe7a5ff939bb4e
Jacob Kyalo <jacobkyalo43@gmail.com>
2023-05-23 17:51:56 +0300

    signup user logic

--- src/pages/signup/index.jsx
+++ src/pages/signup/index.jsx
@@ -1,11 +1,40 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useRegisterUserMutation } from '../../slices/usersApiSlice';
 import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
 import logo from '../../assets/brand/logo.svg';
 
 const index = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [show, setShow] = useState(false);
+  const [signupInfo, setSignupInfo] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+  const navigate = useNavigate();
+  const [registerUser] = useRegisterUserMutation();
+
+  const handleStateChange = (e) => {
+    e.preventDefault();
+    setSignupInfo({ ...signupInfo, [e.target.name]: e.target.value });
+  };
+
+  const handleRegisterUser = async (e) => {
+    e.preventDefault();
+    try {
+      const username = signupInfo.username;
+      const email = signupInfo.email;
+      const password = signupInfo.password;
+      await registerUser({ username, email, password }).unwrap();
+      navigate('/login');
+    } catch (error) {
+      toast.error(error?.data.message || error?.error);
+    }
+  };
+
   const handlePasswordShow = () => {
     setShow((prev) => !prev);
   };
@@ -14,7 +43,9 @@ const index = () => {
       <Link to="/">
         <img src={logo} alt="logo" className="w-full max-w-[300px]" />
       </Link>
-      <form className="block w-full max-w-[400px] rounded-lg bg-white px-4 py-6 shadow-[0_50px_200px_50px_#6d28d9] md:px-6">
+      <form
+        onSubmit={handleRegisterUser}
+        className="block w-full max-w-[400px] rounded-lg bg-white px-4 py-6 shadow-[0_50px_200px_50px_#6d28d9] md:px-6">
         <div className="mb-4">
           <h2 className="text-3xl font-semibold leading-none md:text-4xl">Create Account</h2>
           <p className="text-[14px] opacity-80 md:text-[16px]">Sign up for a free account</p>
@@ -24,6 +55,9 @@ const index = () => {
           <div className="mb-4 rounded-md border border-black p-3">
             <input
               type="text"
+              name="username"
+              value={signupInfo.username}
+              onChange={handleStateChange}
               className="w-full border-none bg-transparent outline-none"
               placeholder="Enter username"
             />
@@ -32,7 +66,14 @@ const index = () => {
         <label htmlFor="email" className="block">
           <span className="mb-2 block text-[17px] font-medium">Email</span>
           <div className="mb-4 rounded-md border border-black p-3">
-            <input type="email" className="w-full border-none bg-transparent outline-none" placeholder="Enter email" />
+            <input
+              type="email"
+              name="email"
+              value={signupInfo.email}
+              onChange={handleStateChange}
+              className="w-full border-none bg-transparent outline-none"
+              placeholder="Enter email"
+            />
           </div>
         </label>
         <label htmlFor="password" className="block">
@@ -40,6 +81,9 @@ const index = () => {
           <div className="mb-8 flex items-center justify-between rounded-md border border-black p-3">
             <input
               type={show ? 'text' : 'password'}
+              name="password"
+              value={signupInfo.password}
+              onChange={handleStateChange}
               className="w-full border-none bg-transparent outline-none"
               placeholder="Enter password"
             />
